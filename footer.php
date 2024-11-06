1709356273570d4f6a158f81dc7fc56d8c1f36c1
poilbois-marco <marco.poilbois@edu.univ-fcomte.fr>
2024-11-06 09:11:47 +0100

    mise en forme du footer

--- footer.php
+++ footer.php
@@ -1,10 +1,15 @@
-<footer>
-       <nav><?php
-         wp_nav_menu ( array (
-           'theme_location' => 'footer-menu'
-         ) ); ?></nav>
-     </footer>
-    </div>
+    <footer>
+      <nav>
+        <?php
+          wp_nav_menu ( array (
+            'theme_location' => 'footer-menu'
+          )); 
+        ?>
+      </nav>
+        <span class="footer-line"></span>
+        <p>Tous droits réservés.</p>
+    </footer>
+
     <?php wp_footer(); ?>
   </body>
 </html>
